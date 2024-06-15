f8fa676ab50da7f26f3cf372ffc56f376036760a
pashupati-singh <119448686+pashupati-singh@users.noreply.github.com>
2024-06-15 14:58:36 +0530

    last

---
+++ App.js
@@ -0,0 +1,13 @@
+import Task from "./Components/Task";
+import TaskList from "./Components/TaskList";
+
+function App() {
+  return (
+    <div className="App">
+      <Task />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
